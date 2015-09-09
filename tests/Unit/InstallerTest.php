86db3b7283d7c3671c6989562c7818569bda173b
clphillips <cody@phillipsdata.com>
2015-09-09 15:07:48 -0700

    More coverage

--- tests/Unit/InstallerTest.php
+++ tests/Unit/InstallerTest.php
@@ -24,6 +24,111 @@ class InstallerTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * @covers ::getInstallPath
+     * @covers ::supportedType
+     * @dataProvider installPathProvider
+     */
+    public function testGetInstallPath($packageType, $expected)
+    {
+        $installer = new Installer($this->io, $this->composer);
+
+        $package = $this->getMockBuilder('Composer\Package\PackageInterface')
+            ->getMock();
+        $package->expects($this->any())
+            ->method('getType')
+            ->will($this->returnValue($packageType));
+        $package->expects($this->any())
+            ->method('getPrettyName')
+            ->will($this->returnValue('vendor/name'));
+
+        $this->assertEquals($expected, $installer->getInstallPath($package));
+    }
+
+    /**
+     * Data provider for testGetInstallPath
+     *
+     * @return array
+     */
+    public function installPathProvider()
+    {
+        return array(
+            array('blesta-plugin', 'plugins/name/'),
+            array('blesta-module', 'components/modules/name/'),
+            array('blesta-gateway-merchant', 'components/gateways/merchant/name/'),
+            array('blesta-gateway-nonmerchant', 'components/gateways/nonmerchant/name/'),
+            array('blesta-invoice-template', 'components/invoice_templates/name/'),
+            array('blesta-report', 'components/reports/name/')
+        );
+    }
+
+    /**
+     * @covers ::getInstallPath
+     * @covers ::supportedType
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetInstallPathException()
+    {
+        $installer = new Installer($this->io, $this->composer);
+
+        $package = $this->getMockBuilder('Composer\Package\PackageInterface')
+            ->getMock();
+        $package->expects($this->any())
+            ->method('getType')
+            ->will($this->returnValue('invalid'));
+        $installer->getInstallPath($package);
+    }
+
+    /**
+     * @covers ::uninstall
+     * @covers ::getInstallPath
+     * @covers ::supportedType
+     */
+    public function testUninstall()
+    {
+        $installer = new Installer($this->io, $this->composer);
+
+        $this->io->expects($this->once())
+            ->method('write');
+
+        $package = $this->getMockBuilder('Composer\Package\PackageInterface')
+            ->getMock();
+        $package->expects($this->any())
+            ->method('getType')
+            ->will($this->returnValue('blesta-plugin'));
+
+        $repo = $this->getMockBuilder('Composer\Repository\InstalledRepositoryInterface')
+            ->getMock();
+        $repo->expects($this->once())
+            ->method('removePackage')
+            ->with($this->equalTo($package));
+        $repo->expects($this->once())
+            ->method('hasPackage')
+            ->with($this->equalTo($package))
+            ->will($this->returnValue(true));
+
+        $installer->uninstall($repo, $package);
+    }
+
+    /**
+     * @covers ::uninstall
+     * @expectedException \InvalidArgumentException
+     */
+    public function testUninstallException()
+    {
+        $installer = new Installer($this->io, $this->composer);
+
+        $package = $this->getMockBuilder('Composer\Package\PackageInterface')
+            ->getMock();
+        $repo = $this->getMockBuilder('Composer\Repository\InstalledRepositoryInterface')
+            ->getMock();
+        $repo->expects($this->once())
+            ->method('hasPackage')
+            ->will($this->returnValue(false));
+
+        $installer->uninstall($repo, $package);
+    }
+
+    /**
      * @covers ::supports
      * @covers ::supportedType
      * @dataProvider packageTypeProvider
